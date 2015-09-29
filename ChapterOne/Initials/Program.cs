6003858e083f93daa2ec73b2e3d812fae42523e3
Devrajsinh <devrajsinh14@gmail.com>
2015-09-29 11:30:13 -0400

    INitials

---
+++ ChapterOne/Initials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      J   FFFFFF");
+            Console.WriteLine("      J   F     ");
+            Console.WriteLine("      J   FFF   ");
+            Console.WriteLine("      J   F     ");
+            Console.WriteLine("J     J   F     ");
+            Console.WriteLine("JJJJJJJ   F     ");
+        }
+    }
+}
